a951ed388476044ed95ad789105a7f9c632798db
Caveney, Meredith <Meredith.Caveney@gtri.gatech.edu>
2022-08-21 14:53:12 -0400

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ float a_weight[512] = {0.000491601, 0.007439144, 0.026456683, 0.057237193, 0.097
 float bp_weight[512];
 
 AudioInputI2S             input;
+//after LMS, put through FFT
 AudioAnalyzeFFT1024       fft;
 AudioConnection           connection(input, 0, fft, 0);
 
@@ -79,6 +80,63 @@ bool readSpl (float * lower, float * upper) {
 
 }
 
+void donlms(double *x, double *d, double *dhat, double *e, double *w, double mu, int N, int xlen)
+{
+
+#ifdef NOTDEFINED
+  // lms(x,d,dhat,e,w,xlen,N,mu)
+  // double    *x;               /* pointer to input data buffer */
+  // double    *d;               /* pointer to desired signal buffer */
+  // double    *w;               /* weight vector */
+  // double    *dhat;            /* estimate of d produced by filter */
+  // double    *e;               /* error */
+  // int        xlen;            /* length of input data buffer */
+  // int        N;               /* filter length */
+  // double     mu;              /* mu = 2 x's the conventional mu */
+#endif
+
+   /* Note: the input data is in a buffer that is actually L+N long */
+   /* so there will be enough data to fill the filter taps as well. */
+   
+   register double s;       /* summer used in filter */
+   register int j,i;        /* loop counters */
+   double *x1;              /* temporary pointer to input data */
+   double *x2;              /* temporary pointer to input data */
+   double *wn,*wn0,*ee;     /* temporary pointer to filter weights */
+   double Em = 1e-20;
+   
+
+/****************************************
+ ******** Matlab equivalent code ********
+   for n = N:length(x),
+     % produce filtered output sample
+     dhat(n) = w * x(n:-1:n-(N-1))';
+     % update the filter coefficients
+     e(n) = d(n) - dhat(n);
+     w = w+2*mu*x(n:-1:n-(N-1))*e(n);
+   end
+*****************************************/
+
+   for(i=0; i<N; i++) {
+     Em += x[i] * x[i];
+   }
+
+   for(i=0; i< xlen; i++)
+   {
+     for(wn = w, x1=x+i, j=0,s=0.0; j<N; j++)
+       s += *(wn++) * *(x1--);
+     x1++;
+     Em += x[i] * x[i] - *x1 * *x1;
+     e[i] = d[i] - s;
+     dhat[i] = s;
+	 // the following multiplication can be made more efficient by using frexpf() to extract the exponent of Em, then use ldexpf() to multiply mu*e[i] by 2^(-exp).  The approximation is fine...
+     s = mu * e[i] / Em;
+	 // The following weight update is for a regular nLMS adaptive filter.  For our system we will need to ensure that the weights stay below 2 in magnitude.  If they get bigger than that, just cap them at that value.
+     for(j=0; j<N; j++)
+       w[j] += s * x[i-j];
+   }
+}
+
 void setup() {
   AudioMemory(50);
   fft.windowFunction(AudioWindowHanning1024);
@@ -105,6 +163,8 @@ void setup() {
     }
   }
 }
+
+
 
 // copied from the fft example:
 void loop() {
